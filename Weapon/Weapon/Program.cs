6c45a691327fa9b5c830f93431a669b289a99cd4
kastetsGitHub <kastets1@gmail.com>
2022-06-22 21:18:14 +0300

    Add encapsulation

---
+++ Weapon/Weapon/Program.cs
@@ -0,0 +1,31 @@
+﻿    class Weapon
+    {
+        public int Damage { get; private set; }
+        public int Bullets { get; private set; }
+    
+        public void Fire(Player player)
+        {
+            player.TakeDamage(Damage);
+            Bullets -= 1;
+        }
+    }
+    
+    class Player
+    {
+        public int Health { get; private set; }
+    
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+    }
+    
+    class Bot
+    {
+        public Weapon Weapon { get; private set; }
+    
+        public void OnSeePlayer(Player player)
+        {
+           Weapon.Fire(player);
+        }
+    }
